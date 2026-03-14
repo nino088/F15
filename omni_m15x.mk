1a4fd6a2bc606477896ab19fd647942e7705b125
kinguser981@gmail.com <Samuel Kendall>
2026-03-14 15:28:46 +0000

    TWRP device tree

---
+++ omni_m15x.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from m15x device
+$(call inherit-product, device/samsung/m15x/device.mk)
+
+PRODUCT_DEVICE := m15x
+PRODUCT_NAME := omni_m15x
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-E156B
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="m15xnndxx-user 13 TP1A.220624.014 E156BXXS5AYA1 release-keys"
+
+BUILD_FINGERPRINT := samsung/m15xnndxx/m15x:13/TP1A.220624.014/E156BXXS5AYA1:user/release-keys
