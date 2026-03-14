6823e3e9aab285f60c440021c3953311e52f7791
nino <pilopilosimon1990@gmail.com>
2026-03-14 23:07:20 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -6,3 +6,11 @@
 #
 
 LOCAL_PATH := device/samsung/m15x
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
