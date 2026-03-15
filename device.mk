d5b5716ca15722095189ef977f316616b241d001
nino <pilopilosimon1990@gmail.com>
2026-03-15 14:36:31 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -15,5 +15,20 @@ PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
 
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-mtkimpl \
+    android.hardware.boot@1.2-mtkimpl.recovery
+
+# Health Hal
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
+    
+# MTK plpath utils
+PRODUCT_PACKAGES += \
+    mtk_plpath_utils \
+    mtk_plpath_utils.recovery
+
 # VNDK
 PRODUCT_TARGET_VNDK_VERSION := 32
