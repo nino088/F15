e0faff75ef8e4a66db3aaafd2373a7cdb33ea8cd
nino <pilopilosimon1990@gmail.com>
2026-03-16 08:46:39 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,16 +1,10 @@
 #
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2024 The Android Open Source Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-LOCAL_PATH := device/samsung/m15x
-
-# Dynamic partitions
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-# fastbootd
+# Fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
@@ -29,6 +23,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     mtk_plpath_utils \
     mtk_plpath_utils.recovery
-
-# VNDK
-PRODUCT_TARGET_VNDK_VERSION := 32
+    
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
