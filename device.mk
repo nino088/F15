ba8129002d79272155c4f5fcd1cce213019b19a2
nino <pilopilosimon1990@gmail.com>
2026-03-17 08:50:02 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -10,9 +10,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Installs developer gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
-# API
-PRODUCT_SHIPPING_API_LEVEL := 33
-
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
