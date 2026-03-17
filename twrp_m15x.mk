58099adfa4a0a97fbec60db979a74c5a416a51e6
nino <pilopilosimon1990@gmail.com>
2026-03-17 08:04:44 +0100

    Update twrp_m15x.mk

--- twrp_m15x.mk
+++ twrp_m15x.mk
@@ -14,10 +14,11 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 # Inherit from m15x device
 $(call inherit-product, device/samsung/m15x/device.mk)
 
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := m15x
 PRODUCT_NAME := twrp_m15x
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-E156B
+PRODUCT_MODEL := Samsung Galaxy f15
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
