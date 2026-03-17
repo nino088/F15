164dd11d7ea7544f28b5d30a11bee136a1b6cf27
nino <pilopilosimon1990@gmail.com>
2026-03-17 09:58:38 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,2 +1,31 @@
-# Inherit from common tree
-$(call inherit-product, device/samsung/mt6835-common/twrp_mt6835-common.mk)
+TARGET_DEVICE := m15x
+
+# API levels
+PRODUCT_SHIPPING_API_LEVEL := 32
+
+# Dynamic
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.1-impl-mock \
+    fastbootd 
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    update_engine \
+    update_engine_sideload \
+    update_verifier \
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
+    
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 32
